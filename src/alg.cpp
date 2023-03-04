1dcc7505dc6925d92f102aaadbab1a42ef4e582f
mlukashov <125198939+mlukashov@users.noreply.github.com>
2023-03-04 23:29:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -21,7 +21,7 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
     double ex = 1;
-    for (uint16_t i = 0; i <= count; i++)
+    for (uint16_t i = 1; i <= count; i++)
         ex = ex + calcItem(x, i);
     return ex;
 }
